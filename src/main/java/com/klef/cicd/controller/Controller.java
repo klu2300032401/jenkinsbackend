500d1b687b010fae1e72e5592f59802636d00e0b
LOHITHA <lohithagutha@gmail.com>
2025-10-22 12:34:11 +0530

    Project v3.0

--- src/main/java/com/klef/cicd/controller/Controller.java
+++ src/main/java/com/klef/cicd/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.klef.cicd.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/jenkinsbackend")
 public class Controller {
 	@GetMapping("/")
 	public String run() {
